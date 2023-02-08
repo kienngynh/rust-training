c08edd855e2842dc96a413199f9ae2b13b277ce6
kienngynh <kien.ngynh@gmail.com>
2023-02-08 20:58:19 +0700

    ok

---
+++ rust-part1/target/debug/deps/rust_part1-e553056bcb582f7d.d
@@ -0,0 +1,5 @@
+/home/kienngynh/GitHub/rust-training/rust-part1/target/debug/deps/rust_part1-e553056bcb582f7d.rmeta: src/main.rs
+
+/home/kienngynh/GitHub/rust-training/rust-part1/target/debug/deps/rust_part1-e553056bcb582f7d.d: src/main.rs
+
+src/main.rs:
