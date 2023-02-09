00a20f293bd49f76391a01c437f666490b440111
kienngynh <kien.ngynh@gmail.com>
2023-02-09 21:29:17 +0700

    ok

--- rust-part3/src/main.rs
+++ rust-part3/src/main.rs
@@ -5,6 +5,18 @@ struct Member {
     _age: u64,
     _active: bool,
 }
+struct Hinhchunhat{
+    dai:u32,
+    rong:u32,
+}
+impl Hinhchunhat {
+    fn dien_tich(&self) -> u32{
+        self.dai * self.rong
+    }
+    fn chua(&self,hinhchunhatkhac:&Hinhchunhat) -> bool{
+        self.dai > hinhchunhatkhac.dai && self.rong > hinhchunhatkhac.rong
+    }
+}
 fn main() {
     let mut _member1 = Member {
         _user_name: String::from("tulip_bk"),
@@ -21,6 +33,13 @@ fn main() {
         27,
     );
     println!("Member 2 is {:#?}", _member2);
+
+    let _kich_thuoc = Hinhchunhat{dai:60,rong:30};
+    println!("Dien tich hinh chu nhat la {}", _kich_thuoc.dien_tich());
+    let _kich_thuoc2 = Hinhchunhat{dai:50,rong:20};
+    let _kich_thuoc3 = Hinhchunhat{dai:70,rong:20};
+    println!("Hinh chu nhat co the chua hinh 2: {}", _kich_thuoc.chua(&_kich_thuoc2));
+    println!("Hinh chu nhat co the chua hinh 3: {}", _kich_thuoc.chua(&_kich_thuoc3));
 }
 
 fn create_new_member(_user_name: String, _email: String, _age: u64) -> Member {
