619c1f31b96df5267b912d0b299e949bb2f21365
kienngynh <kien.ngynh@gmail.com>
2023-02-18 13:03:35 +0700

    ok

--- rust_part4/src/main.rs
+++ rust_part4/src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+enum IpAddressKind {
+    V4(u8,u8,u8,u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+struct _IpAddress {
+    kind: IpAddressKind,
+    address: String,
+}
+impl _IpAddress{
+    fn some_funtion (){
+        println!("Blockchain Development");
+    }
+}
 fn main() {
-    println!("Hello, world!");
+
+    let localhost = IpAddressKind::V4(127,0,0,1);
+    println!("local host = {:#?}",localhost);
 }
