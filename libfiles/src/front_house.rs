fe4f829a9bbb5f963e52b79cc7cf20b23324e65a
kienngynh <kien.ngynh@gmail.com>
2023-02-20 21:51:34 +0700

    ok

---
+++ libfiles/src/front_house.rs
@@ -0,0 +1,10 @@
+pub mod front_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        pub fn seat_at_table() {}
+    }
+    pub mod serving {
+        pub fn take_order() {}
+        pub fn take_payment() {}
+    }
+}
---
+++ libfiles/src/lib.rs
@@ -0,0 +1,38 @@
+use rand::{Rng,RngCore,SeedableRng};
+fn call_order() {}
+pub mod back_house {
+    fn cook_order() {}
+    fn fix_order() {
+        super::call_order();
+        cook_order()
+    }
+    pub struct Breakfast {
+        pub toast: String,
+        pub fruit: String,
+    }
+    pub enum Pre{
+        Soup,
+        Salad,
+    }
+    impl Breakfast {
+        pub fn monday(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                fruit: String::from("banana"),
+            }
+        }
+    }
+}
+mod front_house;
+
+fn eat_at_restaurant() {
+    let mut _order = back_house::Breakfast::monday("Fish");
+    _order.toast = String::from("Chicken");
+    
+    let _order1 = back_house::Breakfast{
+        toast:String::from("Wheat"),
+        fruit:String::from("apple"),
+    };
+
+    let _order2=back_house::Pre::Salad;
+}
