7ace7893f1a41eb09e8ab3ccb675fa2b1015e2a4
kienngynh <kien.ngynh@gmail.com>
2023-02-21 11:59:38 +0700

    ok

--- rust-part6/src/main.rs
+++ rust-part6/src/main.rs
@@ -7,7 +7,10 @@ fn main() {
     _vector2.push(3);
     _vector2.push(4);
     println!("vector 2 = {:?}", _vector2);
-    
-    let four = &_vector1[3];
-    println!("{}",four);
+
+    let _four = &_vector1[4];
+    match _vector1.get(3){
+        Some(_four)=>println!("This is four element = {}",_four),
+        None => println!("This is not four element"),
+    }
 }
