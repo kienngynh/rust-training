9c6cc3963994e665518b36c6e9d6cac84b81763b
kienngynh <kien.ngynh@gmail.com>
2023-02-21 13:35:04 +0700

    ok

--- rust-part6/src/main.rs
+++ rust-part6/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let _array = [1, 2, 3];
-    let _vector1 = vec![1, 2, 3, 4, 5, 6];
+    let mut _vector1 = vec![0,1, 2, 3, 4, 5, 6];
     let mut _vector2: Vec<i32> = Vec::new();
     _vector2.push(1);
     _vector2.push(2);
@@ -8,9 +8,12 @@ fn main() {
     _vector2.push(4);
     println!("vector 2 = {:?}", _vector2);
 
-    let _four = &_vector1[4];
     match _vector1.get(3){
-        Some(_four)=>println!("This is four element = {}",_four),
+        Some(_v)=>println!("This is four element = {}",_v),
         None => println!("This is not four element"),
     }
+    for i in & mut _vector1{
+        *i += 10;
+    }
+    println!("{:?}",_vector1);
 }
