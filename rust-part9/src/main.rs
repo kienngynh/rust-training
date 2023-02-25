e7d0c4dcc21c8464c2b4f79b2f211807172311ac
kienngynh <kien.ngynh@gmail.com>
2023-02-25 18:51:49 +0700

    ok

--- rust-part9/src/main.rs
+++ rust-part9/src/main.rs
@@ -10,3 +10,23 @@ fn get_ref<'a>(parameter1: &'a i32, parameter2: &'a i32) -> &'a i32 {
         parameter2
     }
 }
+#[allow(dead_code)]
+fn test_1(param1: Vec<f64>) -> Vec<f64> {
+    param1
+} // Lifetime don't apply
+#[allow(dead_code)]
+fn test_2<'a>(param1: &'a Vec<f64>) -> &Vec<f64> {
+    param1
+} // Lifetime don't apply
+#[allow(dead_code)]
+fn test_3(param1: &Vec<f64>) -> &Vec<f64> {
+    &param1
+} // Lifetime don't apply
+#[allow(dead_code)]
+fn test_4<'a, 'b: 'a>(param1: i32, param2: &'a str, param3: &'b str, param4: i32) -> &'a str {
+    if param1 == 7 && param4 > 10 {
+        param2
+    } else {
+        param3
+    }
+}
