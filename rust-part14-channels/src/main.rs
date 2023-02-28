ac5816872036c825b3660b77b746016f8fee76cf
kienngynh <kien.ngynh@gmail.com>
2023-02-28 22:35:24 +0700

    ok

--- rust-part14-channels/src/main.rs
+++ rust-part14-channels/src/main.rs
@@ -1,17 +1,33 @@
 use crossbeam_channel::unbounded;
 use std::thread;
+
+enum Message {
+    PrintMsg(String),
+    Sum(i32, i32),
+    Quit,
+}
 fn main() {
     let (sender, receiver) = unbounded();
-    let _thread_1 = thread::spawn(move || match receiver.recv() {
-        Ok(v) => println!("{}", v),
-        Err(e) => println!("{:?}", e),
+    let _thread_1 = thread::spawn(move || loop {
+        match receiver.recv() {
+            Ok(msg) => match msg {
+                Message::PrintMsg(data) => println!("{}", data),
+                Message::Sum(a, b) => println!("{} + {} = {}", a, b, a + b),
+                Message::Quit => {
+                    println!("Quit program");
+                    break;
+                }
+            },
+            Err(e) => {
+                println!("Error data: {:?}", e);
+                break;
+            }
+        }
     });
-    match sender.send("Hello, I am Nguyen Huu Kien!") {
-        Ok(v) => v,
-        _ => (),
-    };
-    match _thread_1.join() {
-        Ok(v) => v,
-        _ => (),
-    };
+    #[allow(unused)]
+    sender.send(Message::PrintMsg(String::from("My name is Kien")));
+    sender.send(Message::Sum(5, 17));
+    sender.send(Message::Quit);
+    //drop(sender);
+    _thread_1.join();
 }
