58ce3fc79071c5766859a93cb549fafd9809fab2
kienngynh <kien.ngynh@gmail.com>
2023-02-28 21:50:31 +0700

    ok

--- rust-part14-channels/src/main.rs
+++ rust-part14-channels/src/main.rs
@@ -2,14 +2,14 @@ use crossbeam_channel::unbounded;
 use std::thread;
 fn main() {
     let (sender, receiver) = unbounded();
+    let _thread_1 = thread::spawn(move || match receiver.recv() {
+        Ok(v) => println!("{}", v),
+        Err(e) => println!("{:?}", e),
+    });
     match sender.send("Hello, I am Nguyen Huu Kien!") {
         Ok(v) => v,
-        _ => ()
+        _ => (),
     };
-    let _thread_1 = thread::spawn(move || match receiver.recv() {
-        Ok(v) => println!("{}",v),
-        Err(e) => println!("{:?}",e),
-    });
     match _thread_1.join() {
         Ok(v) => v,
         _ => (),
