fc7c19493b7528904debb365b0e5f83415422ef2
kienngynh <kien.ngynh@gmail.com>
2023-03-05 08:27:50 +0700

    ok

---
+++ rust-part20-partialeq/src/main.rs
@@ -0,0 +1,47 @@
+use std::cmp::{Ordering, PartialOrd};
+#[derive(PartialEq, PartialOrd)]
+enum Employer {
+    Marketer(i32),
+    Developer(i32),
+}
+#[derive(PartialEq)]
+struct User {
+    id: i32,
+    name: String,
+}
+impl PartialOrd for User {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.name < other.name {
+            Some(Ordering::Less)
+        } else if self.name > other.name {
+            Some(Ordering::Greater)
+        } else {
+            Some(Ordering::Equal)
+        }
+    }
+}
+fn main() {
+    let a = Employer::Marketer(50);
+    let b = Employer::Developer(40);
+    // with Enum, if condition compare index
+    if a > b {
+        println!("true")
+    } else {
+        println!("false")
+    }
+    let c = User {
+        id: 1,
+        name: "AB".to_owned(),
+    };
+    let d = User {
+        id: 2,
+        name: "B".to_owned(),
+    };
+    // with Struct, partialEq,PartialOrd compare value of first key
+    if c < d {
+        println!("true")
+    } else {
+        println!("false")
+    }
+    println!("{:?}", c.partial_cmp(&d));
+}
